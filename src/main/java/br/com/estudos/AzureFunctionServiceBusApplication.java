dd0921ed86a7309ad3c4c504d3f3ca1edd1c90d9
Eduardo Rodrigues <silva.rodriguesedu@icloud.com>
2024-05-23 22:02:56 -0300

    :sparkles: Project Created! - add project structure - another files

---
+++ src/main/java/br/com/estudos/AzureFunctionServiceBusApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureFunctionServiceBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureFunctionServiceBusApplication.class, args);
+	}
+
+}
